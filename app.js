e8138a3fea9f4bd6b99fe4e34589d72d9fbc8f3b
Kumudu Wijewardhana <kumuduwijewardhana@Kumudus-MacBook-Pro.local>
2025-02-13 16:50:21 +0530

    feat: implement subscriptions

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use(errorMiddleware);
 
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,39 +1,39 @@
-import {Router} from "express";
+import { Router } from "express";
 import userRouter from "./user.routes.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/',(req, res) => {
-    res.send({title: 'Get all subscriptions'});
+subscriptionRouter.get("/", (req, res) => {
+  res.send({ title: "Get all subscriptions" });
 });
 
-subscriptionRouter.get('/:id',(req, res) => {
-    res.send({title: 'Get subscription details'});
+subscriptionRouter.get("/:id", (req, res) => {
+  res.send({ title: "Get subscription details" });
 });
 
-subscriptionRouter.post('/',(req, res) => {
-    res.send({title: 'Create subscription'});
+subscriptionRouter.post("/", authorize, createSubscription);
+
+subscriptionRouter.put("/:id", (req, res) => {
+  res.send({ title: "Update subscription" });
 });
 
-subscriptionRouter.put('/:id',(req, res) => {
-    res.send({title: 'Update subscription'});
+subscriptionRouter.delete("/:id", (req, res) => {
+  res.send({ title: "Delete subscription" });
 });
 
-subscriptionRouter.delete('/:id',(req, res) => {
-    res.send({title: 'Delete subscription'});
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
+
+subscriptionRouter.put("/:id/cancel", (req, res) => {
+  res.send({ title: "Cancel subscription" });
 });
 
-subscriptionRouter.get('/user/:id',(req, res) => {
-    res.send({title: 'Get all user subscriptions'});
+subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+  res.send({ title: "Get all upcoming renewals" });
 });
-
-subscriptionRouter.put('/:id/cancel',(req, res) => {
-    res.send({title: 'Cancel subscription'});
-});
-
-subscriptionRouter.get('/upcoming-renewals',(req, res) => {
-    res.send({title: 'Get all upcoming renewals'});
-});
-
 
 export default subscriptionRouter;
