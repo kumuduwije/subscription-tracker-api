aa4fd2f9c8ebbab4edae5a6e51a0468d5f991374
Kumudu Wijewardhana <kumuduwijewardhana@Kumudus-MacBook-Pro.local>
2025-02-13 15:26:07 +0530

    feat: add rate limit

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
-import express from 'express';
-import {PORT} from './config/env.js';
-import userRouter from './routes/user.routes.js';
-import subscriptionRouter from './routes/subscription.routes.js';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import authRouter from "./routes/auth.route.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscription', subscriptionRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
 
-
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Subscription Tracker API!");
-})
+  res.send("Welcome to the Subscription Tracker API!");
+});
 
-app.listen(PORT, async ()  => {
-    console.log(`Subscription Tracker API Running on port ${PORT}`);
+app.listen(PORT, async () => {
+  console.log(`Subscription Tracker API Running on port ${PORT}`);
 
-    await connectToDatabase()
-})
+  await connectToDatabase();
+});
 
 export default app;
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,35 +1,39 @@
 import jwt from "jsonwebtoken";
-import {JWT_SECRET} from "../config/env.js";
+import { JWT_SECRET } from "../config/env.js";
 import User from "../models/user.model.js";
 
+const authorize = async (req, res, next) => {
+  try {
+    let token;
 
-const authorize = async (req,res,next) =>{
-    try{
-        let token;
-
-        if(req.headers.authorization && req.headers.authorization.startsWith("Bearer ")){
-            token = req.headers.authorization.split(" ")[1];
-        }
-
-        //Check if there is no token
-        if(!token){
-            return res.status(401).json({message: 'No token provided (Unauthorized)'});
-        }
-
-        const decoded = jwt.verify(token,JWT_SECRET);
-
-        const user = await User.findById(decoded.userId)
-
-        if(!user){
-            return res.status(401).json({message: 'Unauthorized'});
-
-        }
-        req.user = user;
-        next()
-
-    }catch (error){
-        res.status(401).json({message:"Unauthorized", error:error.message});
+    //Check if there is a token in the headers
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer ")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
     }
-}
+
+    //Check if there is no token
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "No token provided (Unauthorized)" });
+    }
+
+    //Verify the token using the JWT_SECRET
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized", error: error.message });
+  }
+};
 
 export default authorize;
